47240e4653ceecba887b01d5794df030d251f268
gojosatorukz <b.rashiduly@gmail.com>
2026-01-31 18:19:10 +0500

    only blogic

--- services/authService.js
+++ services/authService.js
@@ -1,23 +1,19 @@
-const pool = require('../config/db');
 const bcrypt = require('bcrypt');
+const userRepository = require('../repositories/userRepository'); 
 const profileService = require('./profileService'); 
 
 class AuthService {
     
     async registerUser(email, password) {
-        const userCheck = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        if (userCheck.rows.length > 0) {
+        const existingUser = await userRepository.findByEmail(email);
+        if (existingUser) {
             throw new Error("Пользователь с таким email уже существует");
         }
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUserResult = await pool.query(
-            'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email',
-            [email, hashedPassword]
-        );
-        const newUser = newUserResult.rows[0];
+        const newUser = await userRepository.create(email, hashedPassword);
 
         await profileService.createDefaultProfile(newUser.id, newUser.email);
 
@@ -25,8 +21,7 @@ class AuthService {
     }
 
     async loginUser(email, password) {
-        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        const user = result.rows[0];
+        const user = await userRepository.findByEmail(email);
 
         if (!user) throw new Error("Неверный email или пароль");
 
@@ -37,7 +32,7 @@ class AuthService {
         try {
              userProfile = await profileService.getProfileByUserId(user.id);
         } catch (e) {
-            // no profile = null
+            // null
         }
 
         return {
